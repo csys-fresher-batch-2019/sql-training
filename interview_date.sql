b96611552f45c12be038517fd3b8ec88df9c2d4f
Naresh Kumar <naresh.20191224@gmail.com>
2020-01-02 16:26:57 +0530

    Update interview_date.sql

--- interview_date.sql
+++ interview_date.sql
@@ -1,2 +1,14 @@
 create table interviews( interview_date date, created_date timestamp default current_timestamp,
 constraint interview_date_ck CHECK (  trunc(created_date) <= interview_date ) );
+
+
+
+SQL> insert into interviews (interview_date) values (sysdate);
+
+1 row created.
+
+SQL> insert into interviews (interview_date) values (sysdate-1);
+insert into interviews (interview_date) values (sysdate-1)
+*
+ERROR at line 1:
+ORA-02290: check constraint (NARESH.INTERVIEW_DATE_CK) violated
